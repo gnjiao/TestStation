54773d9e2644d5dd4c85eb9b703c79cddb7fb560
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2018-05-05 17:19:43 +0800

    1. Add projects; 2. Add basic Logger.

---
+++ Utils/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Logger.config", ConfigFileExtension = "config", Watch = true)]
+namespace Utils
+{
+    public class Logger
+    {
+        public static void Info(string info)
+        {
+            ILog log = LogManager.GetLogger("TestLogger");//获取一个日志记录器
+            log.Warn(DateTime.Now.ToString() + ": login success");//写入一条新log
+        }
+    }
+}
