9ed04ea3668c7fa5747055f7f4ea7470983061ef
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2018-05-19 18:13:34 +0800

    Rename JbImage.

--- Image/CirclesFinder.cs
+++ Image/CirclesFinder.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Image
+namespace JbImage
 {
     public class Line
     {
--- ImageTest/Circle.cs
+++ ImageTest/Circle.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Image;
+using JbImage;
 using Utils;
 
 namespace ImageTest
