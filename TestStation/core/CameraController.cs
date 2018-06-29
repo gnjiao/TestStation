a610a4442d180dfaef453b008cd5316188b27aa2
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2018-06-29 23:52:23 +0800

    Refactor：CameraController

---
+++ TestStation/core/CameraController.cs
@@ -0,0 +1,218 @@
+﻿using Hardware;
+using JbImage;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using Utils;
+
+namespace TestStation.core
+{
+    public class CameraController
+    {
+        private Logger _log = new Logger(typeof(CameraController));
+        public Camera mCamera;
+
+        public Bitmap LatestImage;
+        public Bitmap AnalyzedImage;
+
+        public CameraController()
+        {
+        }
+
+        string _filePath;
+        private List<double> _distances = new List<double>();
+        private List<EmguCircleImage> _imgs = new List<EmguCircleImage>();
+        public Result Open(string cameraType)
+        {
+            if (mCamera == null)
+            {
+                switch (cameraType)
+                {
+                    case "Type A":
+                        HardwareSrv.GetInstance().Add(new M8051("Camera"));
+                        break;
+                    case "Type B":
+                        HardwareSrv.GetInstance().Add(new Vcxu("Camera"));
+                        break;
+                    default:
+                        return new Result("Fail", "Unknown camera type");
+                }
+
+                mCamera = HardwareSrv.GetInstance().Get("Camera") as Camera;
+
+                try
+                {
+                    Result ret = mCamera.Execute(new Command("Open"));
+                    if (ret.Id != "Ok")
+                    {
+                        mCamera = null;
+                    }
+                    return ret;
+                }
+                catch (Exception ex)
+                {
+                    _log.Error("Failed to open the camera", ex);
+                    mCamera = null;
+                    return new Result("Fail", ex.ToString());
+                }
+            }
+            else
+            {
+                return new Result("Dummy", $"Camera({mCamera.Name}) is already opened");
+            }
+        }
+        public Result Read(double distance = double.NaN)
+        {
+            if (mCamera != null)
+            {
+                Result ret = mCamera.Execute(new Command("Read", new Dictionary<string, string> { { "Type", "Bmp" } }));
+                if (ret.Id == "Ok")
+                {
+                    InsertImg(ret.Param as Bitmap, distance, true);
+                }
+
+                return ret;
+            }
+            else
+            {
+                return new Result("Fail", "Please open camera first");
+            }
+        }
+        public Result Load(string filename, double distance = double.NaN)
+        {
+            LatestImage = new Bitmap(filename);
+            InsertImg(LatestImage, distance, false);
+            return new Result("Ok");
+        }
+        public Result Analyze(int[] radiusLimits)
+        {
+            EmguCircleImage image = new EmguCircleImage(_filePath, radiusLimits);
+            _imgs.Add(image);
+            image.Count(double.Parse(ConfigurationManager.AppSettings["CountThreshold"]));
+            AnalyzedImage = image.Draw();
+
+            return new Result("Ok");
+        }
+        private Result _calculate()
+        {
+            double[] result = new double[_imgs[0].FilteredCircles.Count];
+            for (int circleId = 0; circleId < result.Length; circleId++)
+            {
+                try
+                {
+                    double[] radius = new double[_imgs.Count];
+
+                    for (int imgId = 0; imgId < radius.Length; imgId++)
+                    {
+                        radius[imgId] = _imgs[imgId].FilteredCircles[circleId].Radius;
+                    }
+                    string output = "";
+                    for (int i = 0; i < radius.Length; i++)
+                    {
+                        output += radius[i].ToString("F2") + ",";
+                    }
+                    if (output.Length > 0)
+                    {
+                        output = output.Substring(0, output.Length - 1);
+                    }
+                    _log.Debug($"Circle{circleId} radius: {output}");
+
+                    result[circleId] = Matlab.CalcWeist2(radius, _distances.ToArray());
+                    _log.Info($"{circleId}: {result[circleId]}" + Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    result[circleId] = double.NaN;
+                    _log.Error($"Failed to calcute weist for {circleId}", ex);
+                }
+            }
+
+            return new Result("Ok");
+        }
+        public Result Calculate()
+        {
+            Result ret;
+
+            if (_imgs.Count > 0 && _imgs.Count == _distances.Count)
+            {
+                ret =  _calculate();
+            }
+            else if (_imgs.Count == 0)
+            {
+                ret = new Result("Fail", "No analyzed images found");
+            }
+            else
+            {
+                ret = new Result("Fail", "Calculation can only apply to images with distance parameter, please re-do the test");
+            }
+
+            _distances.Clear();
+            _imgs.Clear();
+
+            return ret;
+        }
+        public Result Close()
+        {
+            mCamera?.Execute(new Command("Close"));
+            mCamera = null;
+            return new Result("Ok");
+        }
+        private void InsertImg(Bitmap img, double distance, bool shouldSave = true)
+        {
+            if (!double.IsNaN(distance))
+            {
+                _distances.Add(distance);
+            }
+
+            _filePath = @"data/" + $"Img_{distance}_{DateTime.Now.ToString("yyyyMMdd-HHmmss")}.bmp";
+            img.Save(_filePath, ImageFormat.Bmp);
+        }
+        private void ShowParams()
+        {
+        }
+        /* to be obsoleted */
+        private void ProcessWithCircleFinder()
+        {
+            //var rawData = mCamera.Execute(new Command("Read", new Dictionary<string, string> { { "Type", "Raw" } })).Param as Bitmap;
+
+            string resultBmp = Utils.String.FilePostfix(_filePath, "-result");
+            string resultTxt = resultBmp.Replace(".bmp", ".txt");
+
+            Bitmap bmpFile = ImgProcess.Binarize(_filePath);
+            CirclesFinder f = new CirclesFinder(bmpFile);
+
+            LatestImage = f.Draw(resultBmp);
+
+            ImgProcess.Count(f.Rounds);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(resultTxt, true))
+            {
+                file.Write(string.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8}",
+                    "ID", "X", "Y",
+                    "LengthOnX", "DeviationOnX",
+                    "LengthOnY", "DeviationOnY",
+                    "Weight", "DeviationOnWeight") + Environment.NewLine);
+
+                foreach (var round in f.Rounds)
+                {
+                    file.Write(string.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8}",
+                        round.Id.ToString("D3"), round.CenterX, round.CenterY,
+                        round.MaxLenLine.Length, round.LenXDiff.ToString("F4"),
+                        round.EndY - round.StartY, round.LenYDiff.ToString("F4"),
+                        round.Weight.ToString(), round.WeightDiff.ToString("F4")));
+                    file.Write(Environment.NewLine);
+                }
+
+                double radiusStdEv = Utils.Math.StdEv(f.Rounds.Select(x => (double)x.MaxLenLine.Length).ToList());
+                file.Write(string.Format("StdEv of Radius: {0}", radiusStdEv));
+                file.Write(Environment.NewLine);
+
+                double weightStdEv = Utils.Math.StdEv(f.Rounds.Select(x => (double)x.Weight).ToList());
+                file.Write(string.Format("StdEv of Weight: {0}", weightStdEv));
+                file.Write(Environment.NewLine);
+            }
+        }
+    }
+}
